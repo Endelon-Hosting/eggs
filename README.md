71e3c45a5b05590235409fed32efe6c0cd0eddec
Marcel Baumgartner <marcel.kbkm@gmail.com>
2022-10-06 14:02:40 +0200

    Added path of titans

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eggs
 Some custom eggs for pterodactyl
+
+Games:
+
+- The Isle
+- Path Of Titans
---
+++ pathoftitans/install.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+echo "Path Of Titans Egg by Marcel Baumgartner"
+echo "(c) Copyright 2022 - endelon-hosting.de"
+cd /mnt/server
+echo "Installing utils"
+apt update
+apt install -y wget
+echo "Downloading AlderonGamesCmd"
+wget -O AlderonGamesCmd https://launcher-cdn.alderongames.com/AlderonGamesCmd-Linux-x64
+echo "Download complete. Marking as executeable"
+chmod +x AlderonGamesCmd
+echo "Updating files"
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir .
+echo "Creating folder for config files because this game is a weird one"
+mkdir -p /mnt/server/PathOfTitans
+mkdir -p /mnt/server/PathOfTitans/Saved
+mkdir -p /mnt/server/PathOfTitans/Config
+mkdir -p /mnt/server/PathOfTitans/Config/LinuxServer
+echo "Downloading startup script and default config files from Github"
+wget -O /mnt/server/PathOfTitans/Config/LinuxServer/Game.ini https://raw.githubusercontent.com/Endelon-Hosting/eggs/main/pathoftitans/Game.ini
+wget -O /mnt/server/startup.sh https://raw.githubusercontent.com/Endelon-Hosting/eggs/main/pathoftitans/startup.sh
+echo "Install finished"
